2516a4c93c1de19a967987b2fd0fa9f3923a72fb
VladimirDubinin <106279920+VladimirDubinin@users.noreply.github.com>
2022-05-28 11:02:23 +0300

    updated

--- sportrus/php/commdelete.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($_POST['id'])) {
-	$id = $_POST['id'];
-	require('dbconnect.php');
-	$db->query("DELETE FROM comments WHERE comm_id = '$id'");
-	echo "Success";
-}	
-else echo 'Error';
