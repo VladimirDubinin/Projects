d8085b7f6a4f7ed6a7d422d2694f3d088a78032d
VladimirDubinin <106279920+VladimirDubinin@users.noreply.github.com>
2022-05-29 17:26:19 +0300

    updated

--- sportrus/php/newsdelete.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_POST['id'])) {
-	$id = $_POST['id'];
-	require('dbconnect.php');
-	$db->query("DELETE FROM news WHERE id = '$id'");
-	$db->query("DELETE FROM comments WHERE news_id = '$id'");
-	echo "Success";
-}	
-else echo 'Error';
