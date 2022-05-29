dcbb9b79cfb3400c695aeda85ce7989cbe6d4c49
VladimirDubinin <106279920+VladimirDubinin@users.noreply.github.com>
2022-05-29 23:30:21 +0300

    updated

--- sportrus/php/info.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id = $_POST['id'];
-$surn = $_POST['surn'];
-$name = $_POST['name'];
-$lastn = $_POST['lastn'];
-$about = $_POST['about'];
-require('dbconnect.php');
-$db->query("UPDATE users SET surname = '$surn', name = '$name', lastname = '$lastn', about = '$about'  WHERE id = '$id'");
-echo 'Success';
