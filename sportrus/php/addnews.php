cf91392d98322156e1857f5dc7e87d9efaa7e004
VladimirDubinin <106279920+VladimirDubinin@users.noreply.github.com>
2022-05-29 22:32:03 +0300

    updated

---
+++ sportrus/php/addnews.php
@@ -0,0 +1,18 @@
+<?php
+$title = $_POST['title'];
+$theme = $_POST['theme'];
+$short = $_POST['short'];
+$body = $_POST['body'];
+$author = $_POST['author'];
+$img = $_FILES['img'];
+
+$ftype = mb_strtolower(end(explode(".",$img['name'])));
+$goodtypes = ['png','jpg','jpeg'];
+if(in_array($ftype,$goodtypes)) {
+	$path = 'img/news/'.mt_rand(0, 100000).'_'.$img['name'];
+	move_uploaded_file($img['tmp_name'],'../'.$path);	
+	require('dbconnect.php');
+	//echo "INSERT INTO news(title,theme,short,body,author,img) VALUES ('$title','$theme','$short','$body','$author','$path')";
+	$db->query("INSERT INTO news(title,theme,short,body,author,img) VALUES ('$title','$theme','$short','$body','$author','$path')");
+}
+header('Location: http://sportrus/adminpanel.php');
